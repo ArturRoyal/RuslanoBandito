292590701bf4ab4def6e6a829fbfac0c66d4da58
Artur <a.bilyi@koeppern.com>
2025-11-28 13:08:20 +0100

    new

--- import requests.py
+++ import requests.py
@@ -1,7 +1,9 @@
 import requests
 
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SXR8.DEX&outputsize=full&apikey=QJ3OHFW9EIW6MV4R'
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SXR8.DEX&apikey=D455JCEO99F3A1OF'
+# url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SXR8.DEX&outputsize=full&apikey=D455JCEO99F3A1OF'
+
 # url = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=SXR8.DEX&apikey=QJ3OHFW9EIW6MV4R'
 
 r = requests.get(url)
